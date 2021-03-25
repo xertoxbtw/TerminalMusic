409874712ad04faf54393b44e1b25c34f23f73da
xertoxbtw <49755786+xertoxbtw@users.noreply.github.com>
2021-03-25 22:01:03 +0100

    Clear Up

---
+++ TerminalMusic/Entry.cs
@@ -0,0 +1,15 @@
+﻿// Song Entry
+
+namespace TerminalMusic
+{
+    public class Entry
+    {
+        public string Name;
+        public string Path;
+        public Entry(string _name, string _path)
+        {
+            Name = _name;
+            Path = _path;
+        }
+    }
+}
--- TerminalMusic/Program.cs
+++ TerminalMusic/Program.cs
@@ -272,32 +272,4 @@ namespace TerminalMusic
             }
         }
     }
-
-    public static class CustomArray<T>
-    {
-        public static T[] GetColumn(T[,] matrix, int columnNumber)
-        {
-            return Enumerable.Range(0, matrix.GetLength(0))
-                    .Select(x => matrix[x, columnNumber])
-                    .ToArray();
-        }
-
-        public static T[] GetRow(T[,] matrix, int rowNumber)
-        {
-            return Enumerable.Range(0, matrix.GetLength(1))
-                    .Select(x => matrix[rowNumber, x])
-                    .ToArray();
-        }
-    }
-
-    public class Entry
-    {
-        public string Name;
-        public string Path;
-        public Entry(string _name, string _path)
-        {
-            Name = _name;
-            Path = _path;
-        }
-    }
 }
